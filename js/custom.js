b3e4eb9193e83d9f01832f388aef2d5c1a3d9b89
Scott Angus <scott@clearspacecreative.com>
2024-09-05 08:53:26 -0700

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.nighthawk_base = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
